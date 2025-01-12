01d0022da5ca4f52301cb75d141f761d6aac7fd7
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-13 02:10:35 +0900

    bench donot include search initing

--- benches/search.rs
+++ benches/search.rs
@@ -3,6 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use rand::Rng;
 use rust_reversi_core::board::Board;
 use rust_reversi_core::search::AlphaBetaSearch;
+use rust_reversi_core::search::BitMatrixEvaluator;
 use rust_reversi_core::search::Evaluator;
 use rust_reversi_core::search::LegalNumEvaluator;
 use rust_reversi_core::search::MatrixEvaluator;
@@ -10,8 +11,7 @@ use rust_reversi_core::search::PieceEvaluator;
 
 const EPSILON: f64 = 1e-2;
 
-fn alpha_beta4_piece() {
-    let search = AlphaBetaSearch::new(4, Box::new(PieceEvaluator::new()));
+fn play_with_search(search: &AlphaBetaSearch) {
     let mut board = Board::new();
     while !board.is_game_over() {
         if board.is_pass() {
@@ -27,24 +27,15 @@ fn alpha_beta4_piece() {
     }
 }
 
-fn alpha_beta4_legal_num() {
-    let search = AlphaBetaSearch::new(4, Box::new(LegalNumEvaluator::new()));
-    let mut board = Board::new();
-    while !board.is_game_over() {
-        if board.is_pass() {
-            board.do_pass().unwrap();
-        } else {
-            let m = if rand::thread_rng().gen_bool(EPSILON) {
-                board.get_random_move().unwrap()
-            } else {
-                search.get_move(&board).unwrap()
-            };
-            board.do_move(m).unwrap();
-        }
-    }
+fn get_alpha_beta4_piece() -> AlphaBetaSearch {
+    AlphaBetaSearch::new(4, Box::new(PieceEvaluator::new()))
 }
 
-fn alpha_beta4_matrix() {
+fn get_alpha_beta4_legal_num() -> AlphaBetaSearch {
+    AlphaBetaSearch::new(4, Box::new(LegalNumEvaluator::new()))
+}
+
+fn get_alpha_beta4_matrix() -> AlphaBetaSearch {
     let matrix = black_box([
         [1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1],
@@ -56,20 +47,7 @@ fn alpha_beta4_matrix() {
         [1, 1, 1, 1, 1, 1, 1, 1],
     ]);
     let evaluator = MatrixEvaluator::new(matrix);
-    let search = AlphaBetaSearch::new(4, Box::new(evaluator));
-    let mut board = Board::new();
-    while !board.is_game_over() {
-        if board.is_pass() {
-            board.do_pass().unwrap();
-        } else {
-            let m = if rand::thread_rng().gen_bool(EPSILON) {
-                board.get_random_move().unwrap()
-            } else {
-                search.get_move(&board).unwrap()
-            };
-            board.do_move(m).unwrap();
-        }
-    }
+    AlphaBetaSearch::new(4, Box::new(evaluator))
 }
 
 struct CustomEvaluator {}
@@ -79,28 +57,67 @@ impl Evaluator for CustomEvaluator {
     }
 }
 
-fn alpha_beta4_custom() {
-    let search = AlphaBetaSearch::new(4, Box::new(CustomEvaluator {}));
-    let mut board = Board::new();
-    while !board.is_game_over() {
-        if board.is_pass() {
-            board.do_pass().unwrap();
-        } else {
-            let m = if rand::thread_rng().gen_bool(EPSILON) {
-                board.get_random_move().unwrap()
-            } else {
-                search.get_move(&board).unwrap()
-            };
-            board.do_move(m).unwrap();
-        }
-    }
+fn get_alpha_beta4_custom() -> AlphaBetaSearch {
+    AlphaBetaSearch::new(4, Box::new(CustomEvaluator {}))
+}
+
+fn get_alpha_beta4_bitmatrix5() -> AlphaBetaSearch {
+    let masks: Vec<u64> = black_box(vec![
+        0x0000001818000000,
+        0x0000182424180000,
+        0x0000240000240000,
+        0x0018004242001800,
+        0x0024420000422400,
+    ]);
+    let weights: Vec<i32> = black_box(vec![-1, -1, -1, -2, -2]);
+    let evaluator = BitMatrixEvaluator::<5>::new(weights, masks);
+    AlphaBetaSearch::new(4, Box::new(evaluator))
+}
+
+fn get_alpha_beta4_bitmatrix10() -> AlphaBetaSearch {
+    let masks: Vec<u64> = black_box(vec![
+        0x0000001818000000,
+        0x0000182424180000,
+        0x0000240000240000,
+        0x0018004242001800,
+        0x0024420000422400,
+        0x0042000000004200,
+        0x1800008181000018,
+        0x2400810000810024,
+        0x4281000000008142,
+        0x8100000000000081,
+    ]);
+    let weights: Vec<i32> = black_box(vec![-1, -1, -1, -2, -2, -50, 5, 10, -20, 100]);
+    let evaluator = BitMatrixEvaluator::<10>::new(weights, masks);
+    AlphaBetaSearch::new(4, Box::new(evaluator))
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("alpha_beta4_piece", |b| b.iter(alpha_beta4_piece));
-    c.bench_function("alpha_beta4_legal_num", |b| b.iter(alpha_beta4_legal_num));
-    c.bench_function("alpha_beta4_matrix", |b| b.iter(alpha_beta4_matrix));
-    c.bench_function("alpha_beta4_custom", |b| b.iter(alpha_beta4_custom));
+    let alpha_beta4_piece = get_alpha_beta4_piece();
+    let alpha_beta4_legal_num = get_alpha_beta4_legal_num();
+    let alpha_beta4_matrix = get_alpha_beta4_matrix();
+    let alpha_beta4_custom = get_alpha_beta4_custom();
+    let alpha_beta4_bitmatrix5 = get_alpha_beta4_bitmatrix5();
+    let alpha_beta4_bitmatrix10 = get_alpha_beta4_bitmatrix10();
+
+    c.bench_function("alpha_beta4_piece", |b| {
+        b.iter(|| play_with_search(&alpha_beta4_piece))
+    });
+    c.bench_function("alpha_beta4_legal_num", |b| {
+        b.iter(|| play_with_search(&alpha_beta4_legal_num))
+    });
+    c.bench_function("alpha_beta4_matrix", |b| {
+        b.iter(|| play_with_search(&alpha_beta4_matrix))
+    });
+    c.bench_function("alpha_beta4_custom", |b| {
+        b.iter(|| play_with_search(&alpha_beta4_custom))
+    });
+    c.bench_function("alpha_beta4_bitmatrix5", |b| {
+        b.iter(|| play_with_search(&alpha_beta4_bitmatrix5))
+    });
+    c.bench_function("alpha_beta4_bitmatrix10", |b| {
+        b.iter(|| play_with_search(&alpha_beta4_bitmatrix10))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
