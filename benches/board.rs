0233abb89dd70e8f8426a838c91011b578f494f4
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-03-18 13:37:18 +0900

    add bench perft1

--- benches/board.rs
+++ benches/board.rs
@@ -13,8 +13,33 @@ fn play_game() {
     }
 }
 
+fn perft() {
+    let depth: usize = 6;
+    const PERFT_MODE1: [usize; 11] = [
+        1, 4, 12, 56, 244, 1396, 8200, 55092, 390216, 3005288, 24571284,
+    ];
+    fn perft_rec(board: &mut Board, depth: usize) -> usize {
+        if depth == 0 || board.is_game_over() {
+            return 1;
+        } else if board.is_pass() {
+            let mut new_board = board.clone();
+            new_board.do_pass().unwrap();
+            return perft_rec(&mut new_board, depth - 1);
+        }
+        let mut nodes = 0;
+        for mut b in board.get_child_boards().unwrap() {
+            nodes += perft_rec(&mut b, depth - 1);
+        }
+        nodes
+    }
+    let mut board = Board::new();
+    let nodes = perft_rec(&mut board, depth);
+    assert_eq!(nodes, PERFT_MODE1[depth]);
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("play_game", |b| b.iter(play_game));
+    c.bench_function("perft", |b| b.iter(perft));
 }
 
 criterion_group!(benches, criterion_benchmark);
